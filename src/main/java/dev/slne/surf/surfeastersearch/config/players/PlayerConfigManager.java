220dab51eec033959e84e08f4d37fa49b6c325d3
Jo_field <150937665+JoField08@users.noreply.github.com>
2025-04-13 20:39:37 +0200

    refactor: clean up PlayerConfigManager formatting and remove commented-out code

--- src/main/java/dev/slne/surf/surfeastersearch/config/players/PlayerConfigManager.java
+++ src/main/java/dev/slne/surf/surfeastersearch/config/players/PlayerConfigManager.java
@@ -4,50 +4,38 @@ import dev.slne.surf.surfeastersearch.SurfEasterSearch;
 import dev.slne.surf.surfeastersearch.config.ConfigManager;
 import it.unimi.dsi.fastutil.objects.Object2ObjectMap;
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
+
 import java.util.Map;
 import java.util.UUID;
+
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class PlayerConfigManager implements ConfigManager {
-  private static final int MAX_COLLECT_PER_DAY = 3;
-  private final Object2ObjectMap<UUID, PlayerData> playerDataMap;
+    private static final int MAX_COLLECT_PER_DAY = 3;
+    private final Object2ObjectMap<UUID, PlayerData> playerDataMap;
 
-  public PlayerConfigManager() {
-    this.playerDataMap = new Object2ObjectOpenHashMap<>();
-  }
-
-  @Override
-  public void readConfig(@NotNull ConfigurationSection config) {
-    for (String stringUuid : config.getKeys(false)) {
-      final UUID uuid = UUID.fromString(stringUuid);
-      playerDataMap.put(uuid, config.getSerializable(stringUuid, PlayerData.class));
+    public PlayerConfigManager() {
+        this.playerDataMap = new Object2ObjectOpenHashMap<>();
     }
-  }
 
-  @Override
-  public void writeConfig(@NotNull ConfigurationSection config) {
-    for (Map.Entry<UUID, PlayerData> entry : playerDataMap.object2ObjectEntrySet()) {
-      config.set(entry.getKey().toString(), entry.getValue());
+    @Override
+    public void readConfig(@NotNull ConfigurationSection config) {
+        for (String stringUuid : config.getKeys(false)) {
+            final UUID uuid = UUID.fromString(stringUuid);
+            playerDataMap.put(uuid, config.getSerializable(stringUuid, PlayerData.class));
+        }
     }
-  }
-//
-//  public boolean canCollectToday(@NotNull Player player) {
-//    final PlayerData playerData = playerDataMap.get(player.getUniqueId());
-//    final int todayCollected = playerData.getAndIncrementTodayCollected();
-//
-//    return todayCollected < MAX_COLLECT_PER_DAY;
-//  }
-  public PlayerData getPlayerData(@NotNull UUID uuid) {
-    return playerDataMap.computeIfAbsent(uuid, __ -> new PlayerData());
-  }
 
-//  public void resetDailyLimits() {
-//    for (PlayerData playerData : playerDataMap.values()) {
-//      playerData.resetTodayCollected();
-//    }
-//
-//    SurfEasterSearch.getInstance().saveEasterConfig();
-//  }
+    @Override
+    public void writeConfig(@NotNull ConfigurationSection config) {
+        for (Map.Entry<UUID, PlayerData> entry : playerDataMap.object2ObjectEntrySet()) {
+            config.set(entry.getKey().toString(), entry.getValue());
+        }
+    }
+
+    public PlayerData getPlayerData(@NotNull UUID uuid) {
+        return playerDataMap.computeIfAbsent(uuid, __ -> new PlayerData());
+    }
 }
